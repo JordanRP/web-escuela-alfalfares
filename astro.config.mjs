7ce9ce3a72eaf7710c34329775e38346a0f5c6d8
jordanRP <jordan.pizarro.r@gmail.com>
2025-08-06 12:20:47 -0400

    arreglos

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
+//arreglos
 export default defineConfig({
   site: "https://www.escuelaalfalfares.cl",
   integrations: [mdx(), sitemap()],
